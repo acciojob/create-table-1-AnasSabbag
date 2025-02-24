b0ca95587bf76458be9fa1327b0cb1c79fdf22e4
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-02-24 10:04:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function insert_Row() {
     //Write your code here
-  
+	let tab = document.getElementById("sampleTable");
+	let newSt=`<tr><td>New Cell1</td> <td>New Cell2</td></tr>`;
+	tab.innerHTML = newSt+tab.innerHTML;
   
 }
